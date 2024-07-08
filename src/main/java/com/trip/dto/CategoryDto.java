0475e62243cb8b6592a4c5a287dc512e40d14eaa
ubin0821 <hello.git.z9096@naver.com>
2024-07-08 14:37:15 +0900

    240708 국내해외 선택필드 제작중

---
+++ src/main/java/com/trip/dto/CategoryDto.java
@@ -0,0 +1,19 @@
+package com.trip.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class CategoryDto {
+    private Long id;
+    private String itemNm;
+    private Integer price;
+    private String itemDetail;
+    private String sellStatCd;
+    private String category;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+}
---
+++ src/main/java/com/trip/entity/Category.java
@@ -0,0 +1,4 @@
+package com.trip.entity;
+
+public class Category {
+}
