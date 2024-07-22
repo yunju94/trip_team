5688f90f00e5ed79d4a0a6242607ac135711e2f1
yunju94 <qkryj1004@namver.com>
2024-07-22 10:41:48 +0900

    07_22오전_게시판 변경

--- src/main/java/com/trip/controller/MainController.java
+++ src/main/java/com/trip/controller/MainController.java
@@ -5,9 +5,7 @@ package com.trip.controller;
 import com.trip.dto.ItemSearchDto;
 import com.trip.dto.MainItemDto;
 import com.trip.entity.Item;
-import com.trip.entity.Travel;
 import com.trip.service.ItemService;
-import com.trip.service.TravelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -17,7 +15,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -25,7 +22,6 @@ import java.util.Optional;
 public class MainController {
 
     private  final ItemService itemService;
-
 
     @GetMapping(value = "/")
     public String main(ItemSearchDto itemSearchDto, Optional<Integer> page, Model model) {
@@ -36,6 +32,5 @@ public class MainController {
         model.addAttribute("itemSearchDto", itemSearchDto);
         return "main";
     }
-
 }
 
